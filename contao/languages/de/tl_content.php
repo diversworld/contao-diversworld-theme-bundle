ac7d643e0f5a9753e515b4de968c9c5cc3683d0e
diversworld <info@diversworld.eu>
2026-09-06 14:26:37 +0200

    Theme weiter komplettiert.

---
+++ contao/languages/de/tl_content.php
@@ -0,0 +1,7 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_content']['dw_page'] = ['Zu verlinkende Seite','Wählen Sie die Seite aus zu der verlinkt werden soll.'];
+$GLOBALS['TL_LANG']['tl_content']['dw_linkText'] = ['Link-Text','Der Link-Text wird anstelle der Link-Adresse angezeigt.'];
+$GLOBALS['TL_LANG']['tl_content']['dw_linkTitle'] = ['Link-Titel','Der Link-Titel wird als title-Attribut im HTML-Markup eingefügt.'];
+$GLOBALS['TL_LANG']['tl_content']['target'] = ['In einem neuen Fenster öffnen','Den Link in einem neuen Browserfenster öffnen.'];
+$GLOBALS['TL_LANG']['tl_content']['dw_subHeadline'] = ['Unterüberschrift','Hier können Sie dem Inhaltselement eine Unterüberschrift hinzufügen.'];
---
+++ contao/languages/en/default.php
@@ -0,0 +1,17 @@
+<?php
+
+$GLOBALS['TL_LANG']['CTE']['themeDw'] = 'Diversworld Theme';
+$GLOBALS['TL_LANG']['CTE']['sliderElement'][0] = 'Slider-Element';
+
+$GLOBALS['TL_LANG']['tl_article']['th_tags']['DW01/01'] = 'Kopfzeile / 01';
+$GLOBALS['TL_LANG']['tl_article']['th_tags']['DW01/02'] = 'Kopfzeile / 02';
+$GLOBALS['TL_LANG']['tl_article']['th_tags']['DW02/01'] = 'Fußzeile / 01';
+$GLOBALS['TL_LANG']['tl_article']['th_tags']['DW02/02'] = 'Fußzeile / 02';
+$GLOBALS['TL_LANG']['tl_article']['th_tags']['DW02/03'] = 'Fußzeile / 03';
+$GLOBALS['TL_LANG']['tl_article']['th_tags']['DW02/04'] = 'Fußzeile / 04';
+$GLOBALS['TL_LANG']['tl_article']['th_tags']['DW02/05'] = 'Fußzeile / 05';
+
+$GLOBALS['TL_LANG']['MOD']['dwTheme'][0] = "Diversworld Theme";
+$GLOBALS['TL_LANG']['MOD']['dwThemeSetup'][0] = "Diversworld Theme Setup";
+
+$GLOBALS['TL_LANG']['dw']['event_more_link'] = "Show event details";
