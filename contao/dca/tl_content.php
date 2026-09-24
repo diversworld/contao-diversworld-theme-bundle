d1e937348e510c803cfb4ddec281abf2b25575de
diversworld <info@diversworld.eu>
2026-09-24 12:50:26 +0200

    .

--- contao/dca/tl_content.php
+++ contao/dca/tl_content.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use Contao\CoreBundle\DataContainer\PaletteManipulator;
+
 $GLOBALS['TL_DCA']['tl_content']['palettes']['dw_feature'] = '
     {type_legend},type,dwIcon;
     {headline_legend:hide},headline;
@@ -16,6 +18,15 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['dwIcon'] = [
     'exclude' => true,
     'search' => true,
     'inputType' => 'text',
-    'eval' => ['mandatory' => true, 'maxlength' => 64, 'tl_class' => 'w50'],
+    'eval' => ['mandatory' => false, 'maxlength' => 64, 'tl_class' => 'w50'],
     'sql' => ['type' => 'string', 'length' => 64, 'default' => ''],
 ];
+
+// Allow the core "text" content element to optionally show an icon (Font Awesome
+// class) above the headline, so icon+text combinations no longer require the
+// custom dw_feature element.
+PaletteManipulator::create()
+    ->addLegend('icon_legend', 'text_legend', PaletteManipulator::POSITION_BEFORE)
+    ->addField('dwIcon', 'icon_legend', PaletteManipulator::POSITION_APPEND)
+    ->applyToPalette('text', 'tl_content')
+;
